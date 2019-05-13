1bd81b67e10d571472a96f55162c8e77e4c959bc
John Kuefler <john@dev-squared.com>
2019-05-13 16:26:07 -0500

    Added broken access control example

---
+++ OwaspDemo/Controllers/BrokenAccessControl/BrokenAccessControlAfterController.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using OwaspDemo.Data;
+
+namespace OwaspDemo.Controllers.BrokenAccessControl
+{
+    public class BrokenAccessControlAfterController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BrokenAccessControlAfterController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("~/v2/users/me")]
+        public IActionResult Profile()
+        {
+            var user = _context.Logins.First();
+
+            return Ok(new
+            {
+                user.Name,
+                user.Email
+            });
+        }
+
+        [HttpGet("~/v2/users/")]
+        public IActionResult Users()
+        {
+            if (!User.IsInRole("Admin"))
+            {
+                return Unauthorized();
+            }
+
+            var users = _context.Logins.ToList();
+
+            return Ok(users);
+        }
+    }
+}
---
+++ OwaspDemo/Controllers/BrokenAccessControl/BrokenAccessControlBeforeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OwaspDemo.Data;
+
+namespace OwaspDemo.Controllers.BrokenAccessControl
+{
+    public class BrokenAccessControlBeforeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BrokenAccessControlBeforeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("~/users/me")]
+        public IActionResult Profile()
+        {
+            var user = _context.Logins.First();
+
+            return Ok(new
+            {
+                user.Name,
+                user.Email
+            });
+        }
+
+        [HttpGet("~/users")]
+        public IActionResult Users()
+        {
+            var users = _context.Logins.ToList();
+
+            return Ok(users);
+        }
+    }
+}
